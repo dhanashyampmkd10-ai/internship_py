b32e35dd41bca630303fa1184e353d214ed7b839
Dhanashyam <dhanashyampmkd10@gmail.com>
2026-06-11 07:09:22 +0530

    added day2 folder

--- main.py
+++
@@ -1,7 +0,0 @@
-from utils import greet, calculate_grade
-print(greet("dhanashyam"))
-marks = 85
-grade = calculate_grade(marks)
-print(f"Marks: {marks}")
-print(f"Grade: {grade}")
-print("utils.py imported successfully!")
