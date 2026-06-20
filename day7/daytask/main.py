b4b08954f7a2a1260e29188ba7b5793dceb1317f
DHANASHYAM P <dhanashyampmkd10@gmail.com>
2026-06-20 09:48:13 +0530

    Delete day7 directory

--- day7/daytask/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from routers.tasks import router
-from database import create_table
-
-app = FastAPI()
-
-create_table()
-
-app.include_router(router)
--- day7/min3/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from database import create_table
-from routers.tasks import router
-
-app = FastAPI()
-
-create_table()
-
-app.include_router(router)
