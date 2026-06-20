46e5f458533700b92c2332b533947d3145309f42
Dhanashyam <dhanashyampmkd10@gmail.com>
2026-06-20 12:16:43 +0530

    Restore day5 task files

---
+++ day5/daytask/schemas.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+
+
+class TaskCreate(BaseModel):
+    title: str
+    description: str
+    completed: bool = False
+
+
+class TaskUpdate(BaseModel):
+    title: str
+    description: str
+    completed: bool
+
+
+class TaskResponse(BaseModel):
+    id: int
+    title: str
+    description: str
+    completed: bool
