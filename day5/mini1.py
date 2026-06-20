ef48ffdd53b3d666ecd0c36b7b8f5bbda5a86c6b
DHANASHYAM P <dhanashyampmkd10@gmail.com>
2026-06-20 09:48:35 +0530

    Delete day5 directory

--- day5/mini1.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/hello/{name}")
-def hello(name: str):
-    return {"message": f"Hello, {name}!"}
--- day5/mini2.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-app = FastAPI()
-@app.get("/square/{n}")
-def square(n:int):
-    return{
-        "input":n,
-        "result":n*n
-    }
